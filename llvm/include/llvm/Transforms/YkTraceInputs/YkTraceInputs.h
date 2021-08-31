31964d6ea8660bbe52012c22f4ec3a419ed18d3d
Edd Barrett <vext01@gmail.com>
2021-08-31 17:03:16 +0100

    Locate traced section.

--- llvm/include/llvm/Transforms/YkTraceInputs/YkTraceInputs.h
+++ llvm/include/llvm/Transforms/YkTraceInputs/YkTraceInputs.h
@@ -1,3 +1,5 @@
+// FIXME: make this file look like other LLVM passes. Comments etc.
+
 #ifndef LLVM_TRANSFORMS_YKTRACEINPUTS_YKTRACEINPUTS_H
 #define LLVM_TRANSFORMS_YKTRACEINPUTS_YKTRACEINPUTS_H
 
@@ -5,6 +7,8 @@
 
 namespace llvm {
   class YkTraceInputsPass : public PassInfoMixin<YkTraceInputsPass> {
+    //void injectInputs(CallInst *StartInst, CallInst *StopInst);
+
     public:
       PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
   };
--- llvm/lib/Transforms/YkTraceInputs/YkTraceInputs.cpp
+++ llvm/lib/Transforms/YkTraceInputs/YkTraceInputs.cpp
@@ -1,25 +1,64 @@
 #include "llvm/Transforms/YkTraceInputs/YkTraceInputs.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/IR/Dominators.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Module.h"
+#include "llvm/Analysis/PostDominators.h"
 
 #define DEBUG_TYPE "yk-trace-inputs"
 
 using namespace llvm;
 
-PreservedAnalyses YkTraceInputsPass::run(Module &M, ModuleAnalysisManager &AM) {
-  errs() << "My module pass\n";
+//void YkTraceInputsPass::injectInputs(CallInst *StartInst, CallInst *StopFunc) {
+//  //errs() << ">>> " << F->getName() << "\n";
+//}
 
+PreservedAnalyses YkTraceInputsPass::run(Module &M, ModuleAnalysisManager &AM) {
   Function *StartTracingFunc = M.getFunction("__yktrace_start_tracing");
   if (StartTracingFunc == nullptr)
     return PreservedAnalyses::all();
 
-  errs() << "Used by:\n";
+  Function *StopTracingFunc = M.getFunction("__yktrace_stop_tracing");
+  assert(StopTracingFunc != nullptr);
+
   for (User *U: StartTracingFunc->users()) {
     if (!isa<CallInst>(U))
       continue;
-    U->dump();
+
+    // Identify the traced section of code.
+    CallInst *StartInst = cast<CallInst>(U);
+    Function *Caller = StartInst->getFunction();
+    CallInst *StopInst = nullptr;
+    for (auto &BB: *Caller) {
+      for (auto &I: BB) {
+        if (isa<CallInst>(&I)) {
+            CallInst *CI = cast<CallInst>(&I);
+            Function *CF = CI->getCalledFunction();
+            if (CF != StopTracingFunc)
+              continue;
+            // We assume that any given function only has one traced section.
+            // FIXME early return if not built with assertions.
+            assert(StopInst == nullptr);
+            StopInst = CI;
+        }
+      }
+    }
+    assert(StopInst != nullptr);
+
+    // FIXME write tests for these assertions.
+
+    // Check that to get to `StopInst`, you must have first executed `StartInst`.
+    DominatorTree DT(*Caller);
+    assert(DT.dominates(StartInst, StopInst));
+
+    // Check that there's no way to execute `StartInst` without executing
+    // `StopInst`.
+    PostDominatorTree PDT(*Caller);
+    assert(PDT.dominates(StopInst, StartInst));
+
+    StartInst->dump();
+    StopInst->dump();
   }
   return PreservedAnalyses::all();
 }
